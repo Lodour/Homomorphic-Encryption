6fad116cde97a65b52682dc5e9b272271e320ceb
Yue Gao <jjmangogao@gmail.com>
2018-12-11 17:22:23 -0600

    Initialize static variables.

--- include/random.h
+++ include/random.h
@@ -13,11 +13,11 @@
 class Random {
  public:
   static std::vector<long> Vector(std::size_t size);
+  static long dice_long();
 
  protected:
-  static std::int64_t seed;
-  static std::default_random_engine random_engine;
-  static std::function<long()> dice_int;
+  static std::int64_t seed();
+  static const std::default_random_engine &random_engine();
 };
 
 #endif //HOMOMORPHIC_ENCRYPTION_RANDOM_H
--- src/random.cpp
+++ src/random.cpp
@@ -1,19 +1,27 @@
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "cert-err58-cpp"
 //
 // Created by Roll on 2018-11-28.
 //
 
-#include "random.h"
-
-std::int64_t Random::seed = std::chrono::system_clock::now().time_since_epoch().count();
-std::default_random_engine Random::random_engine = std::default_random_engine(Random::seed);
-std::function<long()> Random::dice_int = std::bind(std::uniform_int_distribution<long>(0, 1000), Random::random_engine);
+#include <random.h>
 
 std::vector<long> Random::Vector(std::size_t size) {
   std::vector<long> vec(size);
-  std::generate(vec.begin(), vec.end(), Random::dice_int);
+  std::generate(vec.begin(), vec.end(), Random::dice_long);
   return vec;
 }
 
-#pragma clang diagnostic pop
+long Random::dice_long() {
+  static auto _dice = std::bind(std::uniform_int_distribution<long>(0, 1000), Random::random_engine());
+  return _dice();
+}
+
+std::int64_t Random::seed() {
+  static auto _seed = std::chrono::system_clock::now().time_since_epoch().count();
+  return _seed;
+}
+
+const std::default_random_engine &Random::random_engine() {
+  static auto _engine = std::default_random_engine(static_cast<unsigned int>(Random::seed()));
+  return _engine;
+}
+
